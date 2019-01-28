5869aaf84be203d4fddeb2fd79bdd8a3d82297b4
Pedro Mangabeira <pedro@getonce.com>
2019-01-28 17:53:55 +0000

    Remove logging APP ID error

--- lib/open_exchange_rates/client.ex
+++ lib/open_exchange_rates/client.ex
@@ -7,9 +7,11 @@ defmodule OpenExchangeRates.Client do
   @app_id Application.get_env(:open_exchange_rates, :app_id)
 
   @doc false
-  @spec get_latest() :: {:ok, Map.t} | {:error, String.t}
+  @spec get_latest() :: {:ok, Map.t()} | {:error, String.t()}
   def get_latest do
-    response = HTTPoison.get("https://openexchangerates.org/api/latest.json?base=USD&app_id=#{@app_id}")
+    response =
+      HTTPoison.get("https://openexchangerates.org/api/latest.json?base=USD&app_id=#{@app_id}")
+
     case response do
       {:ok, %HTTPoison.Response{body: json, status_code: 200}} -> json |> handle_api_success
       {:ok, %HTTPoison.Response{body: json}} -> json |> handle_api_error
@@ -25,9 +27,10 @@ defmodule OpenExchangeRates.Client do
   defp handle_api_error(body) do
     case Poison.decode(body) do
       {:ok, data} ->
-        if data["message"] == "invalid_app_id", do: Logger.error("OpenExchangeRates: The app id `#{@app_id}` is not valid !")
         {:error, data["description"]}
-      _ -> {:error, "Could not parse the JSON : #{inspect body}"}
+
+       ->
+        {:error, "Could not parse the JSON : #{inspect(body)}"}
     end
   end
 end
